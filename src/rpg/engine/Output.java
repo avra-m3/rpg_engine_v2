319304f3a4b8f937b8bcec7bf7d506213762cd69
Hammer-Inc <avramiHammer@gmail.com>
2016-10-23 18:17:27 +1100

    Added Docstrings to Output.java

--- src/rpg/engine/Output.java
+++ src/rpg/engine/Output.java
@@ -14,12 +14,22 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Output {
+/**
+ * Class: Output;
+ * Handles all file output functions, mainly save states.
+ */
+class Output {
     static final String SAVE_PATH = "save.dat";
 
     private ArrayList[] saveData = new ArrayList[9];
     private String[] saveNames = new String[9];
     private Integer lastSaveSlot = -1;
+
+
+    /**
+     * &gtprimitive&lt boolean isEmpty
+     * @return whether or not there are any saves loaded
+     */
     boolean isEmpty()
     {
         for(ArrayList a:saveData) {
@@ -29,6 +39,11 @@ public class Output {
         }
         return true;
     }
+
+    /**
+     * String getSaveList
+     * @return A full list of all the save slots and their corresponding names
+     */
     String[] getSaveList()
     {
         String[] result = saveNames;
@@ -37,24 +52,45 @@ public class Output {
                 result[x] = "empty";
         return result;
     }
+
+    /**
+     * String getSaveList
+     * @param slot overloads the previous function to only return a single slot
+     * @return the name of the slot or "empty" if it isn't in use
+     */
     String getSaveList(Integer slot)
     {
         if(saveNames[slot] == null)
             return "empty";
         return saveNames[slot];
     }
+
+    /**
+     * Integer getLastSaveSlot
+     * @return The value of the save file's last modified save
+     */
     Integer getLastSaveSlot()
     {
         return this.lastSaveSlot;
     }
+
+    /**
+     * String[Array] getLastSave
+     * @return The last modified save data according to the file
+     */
     String[] getLastSave()
     {
         return getSave(getLastSaveSlot());
     }
+
+    /**
+     * void loadSaves
+     * Loads the save information from the SAVE_PATH file and then reads it into memory.
+     */
     void loadSaves()
     {
         try {
-            if(!Files.exists(Paths.get(this.SAVE_PATH)))
+            if(!Files.exists(Paths.get(SAVE_PATH)))
                 return;
             String[] arr = read(SAVE_PATH);
             Integer index = null;
@@ -67,7 +103,7 @@ public class Output {
                     continue;
                 }
                 if(index == null) continue;
-                if (name.equals(null)) {
+                if (name == null) {
                     name = line;
                     saveNames[index] = name;
                     continue;
@@ -79,7 +115,14 @@ public class Output {
             e.printStackTrace();
         }
     }
-    static String[] read(String filepath) throws IOException
+
+    /**
+     * String[Array] read
+     * @param filepath the file to read
+     * @return A String array of the file given
+     * @throws IOException needs to throw as it interfaces with FileReader
+     */
+    private static String[] read(String filepath) throws IOException
     {
         String[] result;
         String line;
@@ -89,6 +132,12 @@ public class Output {
         file.close();
         return result;
     }
+
+    /**
+     * String[Array] getSave.
+     * @param slot the save slot to return.
+     * @return The save data associated with that slot.
+     */
     String[] getSave(Integer slot) {
         return (String[]) this.saveData[slot].toArray();
     }
